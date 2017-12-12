6e792245df15f2fb4055713a0caf4f665f951231
entrptaher <entrptaher@gmail.com>
2017-12-12 13:52:27 +0600

    Init

--- render.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs')
-var marked = require('marked')
-var mustache = require('mustache')
-
-var template = fs.readFileSync('./index.mustache').toString()
-var readme = fs.readFileSync('./README.md').toString()
-fs.writeFileSync('./index.html', mustache.render(template, {content: marked(readme)}))
